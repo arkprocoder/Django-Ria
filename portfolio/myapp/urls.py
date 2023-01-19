d03ae5854b75aaabe2e816b677db8a329380b973
arkprocoder <arkprocoder@gmail.com>
2023-01-19 11:13:41 +0530

    signup

--- portfolio/myapp/urls.py
+++ portfolio/myapp/urls.py
@@ -4,5 +4,7 @@ from myapp import views
 urlpatterns = [
     path('',views.Home,name="Home"),
     path('skill',views.Skill,name="skill"),
-    path('projects',views.Projects,name="Projects")
+    path('projects',views.Projects,name="Projects"),
+    path('signup',views.HandleSignup,name="HandleSignup"),
+    path('login',views.HandleLogin,name="HandleLogin"),
 ]
--- portfolio/myapp/views.py
+++ portfolio/myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def Home(request):
@@ -9,3 +11,30 @@ def Skill(request):
 
 def Projects(request):
     return render(request, "projects.html")
+
+def HandleSignup(request):
+    if request.method=="POST":
+        # print("i am post request")
+        first_name=request.POST.get('fname')
+        last_name=request.POST.get('lname')
+        email=request.POST.get('email')
+        user_password=request.POST.get('pass1')
+        confirm_password=request.POST.get('pass2')
+        if user_password!=confirm_password:
+            return HttpResponse("password not matching")
+        
+        if len(user_password)<5:
+            return HttpResponse("password must be greater then 5")
+
+        myuser=User.objects.create_user(email,email,user_password)
+        myuser.first_name=first_name
+        myuser.last_name=last_name
+        myuser.save()
+        return redirect('/login')
+        # print(first_name,last_name,email,user_password,confirm_password)
+
+    # print("i am a function request")
+    return render(request, "signup.html")
+
+def HandleLogin(request):
+    return render(request, "login.html")
